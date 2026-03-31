a7c70d5b0f6686ce076aaa624567a641f9546bae
Tirtho <thomas.tirole@ik.me>
2026-03-31 11:52:30 +0200

    PhysicsMaterial2D physics + deathZone

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,30 +1,23 @@
-using System.IO.Compression;
 using UnityEngine;
-using System.Collections;
 
 public class Ball : MonoBehaviour
 {
     public float ballSpeed = 10f;
     private int directionY = 1;
+    private Vector2 initialPosition;
     private Vector2 playerPos;
     private Vector2 ballPos;
     private Rigidbody2D rig;
-    private bool canBounce = true;
     private Vector2 currentDirection;
+
+    public GameObject player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        initialPosition = transform.position;
         rig = GetComponent<Rigidbody2D>();
-        ballPos = transform.position;
-        currentDirection = new Vector2(0f, 1f); // vers le haut
-        rig.linearVelocity = currentDirection * ballSpeed;
-    }
-
-    IEnumerator ResetBounce()
-    {
-        yield return new WaitForSeconds(0.01f);
-        canBounce = true;
+        InitLoad();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -40,27 +33,27 @@ public class Ball : MonoBehaviour
             currentDirection = new Vector2(deltaPos * ballSpeed, directionY * ballSpeed).normalized;
             rig.linearVelocity = currentDirection * ballSpeed;
         }
-        if(collision.gameObject.CompareTag("Wall"))
+        if(collision.gameObject.CompareTag("Brick"))
         {
-            //TODO: rebonds sur les murs
-            //rig.linearVelocity = new Vector2(rig.linearVelocity.x * -1, rig.linearVelocity.y);
-            currentDirection = new Vector2(currentDirection.x * -1, currentDirection.y);
-            rig.linearVelocity = currentDirection * ballSpeed;
-        }
-        if(collision.gameObject.CompareTag("TopWall"))
-        {
-            //TODO: rebonds sur les briques et destruction
-            currentDirection = new Vector2(currentDirection.x, currentDirection.y * -1);
-            rig.linearVelocity = currentDirection * ballSpeed;
-        }
-        if(collision.gameObject.CompareTag("Brick") && canBounce)
-        {
-            canBounce = false;
-            //TODO: rebonds sur les briques et destruction
-            currentDirection = new Vector2(currentDirection.x, currentDirection.y * -1);
-            rig.linearVelocity = currentDirection * ballSpeed;
             Destroy(collision.gameObject);
-            StartCoroutine(ResetBounce());
         }
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("DeathZone"))
+        {
+            //TODO: ajouter une méthode pour gérer la mort du joueur et donc baisser le nombre de vie et réinitialiser la position de la balle
+            transform.position = player.transform.position + new Vector3(0f, 0.5f, 0f); // position de départ au dessus du player
+            InitLoad();
+        }
+    }
+
+    void InitLoad()
+    {
+        ballPos = transform.position;
+        currentDirection = new Vector2(0f, 1f); // vers le haut
+        rig.linearVelocity = currentDirection * ballSpeed;
+    }
+
 }
